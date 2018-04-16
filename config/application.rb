974d3fcb948786568636ea458b8e013337b433fe
tkmru <i.am.tkmru@gmail.com>
2018-04-16 16:22:09 +0900

    goodbye coffee script

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,15 @@ module Webapp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+      g.test_framework :rspec,
+        fixtures: true,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        controller_specs: true,
+        model_specs: true,
+        request_specs: false
+      g.fixture_replacement :factory_girl, dir: "spec/factories"
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
